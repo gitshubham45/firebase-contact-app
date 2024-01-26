03055557b968193b3255f83bbe033834bad6133e
gitshubham45 <shubham16290@gmail.com>
2024-01-26 21:14:08 +0530

    type added to firebase logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@
 const Navbar = () => {
     return (
         <div className="h-[60px] bg-white m-4 my-4 rounded-lg flex justify-center text-xl font-medium items-center gap-2">
-                <img src="../public/firebase.svg" />
+                <img type="image/svg+xml" src="../public/firebase.svg" />
                 <h1>Firebase Contact App</h1>  
         </div>
     )
